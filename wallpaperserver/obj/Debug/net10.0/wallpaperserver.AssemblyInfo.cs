44bf495daf116db42152a119f266397a4dd0b272
Gokulashik <gokulashik222@gmail.com>
2026-06-27 00:20:30 +0530

    added a new background with code that has a looping image as placeholder for now

--- wallpaperserver/obj/Debug/net10.0/wallpaperserver.AssemblyInfo.cs
+++ wallpaperserver/obj/Debug/net10.0/wallpaperserver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wallpaperserver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c7aa0012a6a965d066a925b18fbaa608020e11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7bfcdd2d1b95a5c1712e981d96b10a1f2788f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("wallpaperserver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wallpaperserver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
